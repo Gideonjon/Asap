1771c39653a8836a751a6c03f797ad1e73ff377e
Gideonjon <gideonjones63@gmail.com>
2024-10-25 22:35:43 +0100

    authenticaton-screens-completed

--- app/src/main/java/com/example/asap/CreateAccount.kt
+++ app/src/main/java/com/example/asap/CreateAccount.kt
@@ -1,59 +1,157 @@
 package com.example.asap
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Patterns
+import android.util.Patterns.EMAIL_ADDRESS
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.navigation.Navigation
+import com.example.asap.databinding.FragmentCreateAccountBinding
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [CreateAccount.newInstance] factory method to
- * create an instance of this fragment.
- */
 class CreateAccount : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
-
+    private var _binding: FragmentCreateAccountBinding? = null
+    private val binding get() = _binding!!
+    private lateinit var auth : FirebaseAuth
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_create_account, container, false)
+        _binding = FragmentCreateAccountBinding.inflate(inflater, container, false)
+        val view = binding.root
+
+        binding.getStarted.setBackgroundResource(R.drawable.btn_deep_btn)
+
+        auth = FirebaseAuth.getInstance()
+
+        binding.getStarted.setOnClickListener {
+
+            if (!Patterns.EMAIL_ADDRESS.matcher(binding.emailEt.text.toString()).matches()) {
+                binding.email.error = "Invalid Email"
+            }
+            if (binding.passwordEt.text.toString().length == 8) {
+
+                binding.password.error = "Password must be at least 8 characters"
+            }
+            if (!binding.passwordEt.text.toString().contains(
+                    Regex("(?=.*[a-z])(?=.*[0-9])(?=.*[A-Z])")
+                )
+            ) {
+
+                binding.password.error =
+                    "Password must contain at least one uppercase letter, one lowercase letter, and one number"
+            } else {
+                auth.createUserWithEmailAndPassword(binding.emailEt.text.toString(),binding.passwordEt.text.toString()
+                )
+                    .addOnCompleteListener {
+                            if (it.isSuccessful) {
+                                Toast.makeText(
+                                    requireContext(),
+                                    "Account Created,",
+                                    Toast.LENGTH_SHORT
+                                ).show()
+                                Navigation.findNavController(view)
+                                    .navigate(R.id.action_createAccount_to_personalInformation)
+                            } else {
+                                Toast.makeText(
+                                    requireContext(),
+                                    "Account Not Created",
+                                    Toast.LENGTH_SHORT
+                                ).show()
+                            }
+                        }
+
+                    }
+
+            }
+
+
+        binding.arrowBack.setOnClickListener {
+            Navigation.findNavController(view)
+                .navigate(R.id.action_createAccount_to_onboardingScreen)
+
+        }
+
+
+        val textWatcher = object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+
+                if (!Patterns.EMAIL_ADDRESS.matcher(binding.emailEt.text.toString()).matches()) {
+                    binding.email.error = "Incorrect Email"
+                    binding.getStarted.setBackgroundResource(R.drawable.btn_deep_btn)
+                    binding.email.requestFocus()
+                } else {
+                    binding.email.error = "Correct Email"
+                    binding.getStarted.setBackgroundResource(R.drawable.btn_deep_btn)
+
+                }
+                if (binding.passwordEt.text.toString().length == 8) {
+                    binding.password.error = "Password must be at least 8 characters"
+                    binding.getStarted.setBackgroundResource(R.drawable.btn_deep_btn)
+                    binding.password.requestFocus()
+                } else {
+                    binding.password.error = "8 digits complete"
+                    binding.getStarted.setBackgroundResource(R.drawable.btn_deep_btn)
+
+                }
+                if (!binding.passwordEt.text.toString().contains(
+                        Regex("(?=.*[a-z])(?=.*[0-9])(?=.*[A-Z])")
+                    )
+                ) {
+                    binding.getStarted.setBackgroundResource(R.drawable.btn_deep_btn)
+                    binding.password.error =
+                        "Password must contain at least one uppercase letter, one lowercase letter, and one number"
+                } else {
+                    binding.getStarted.setBackgroundResource(R.drawable.btn_deep_btn)
+                    binding.password.error =
+                        "Password must contain at least one uppercase letter, one lowercase letter, and one number"
+
+                }
+                if (!Patterns.EMAIL_ADDRESS.matcher(binding.emailEt.text.toString())
+                        .matches() && binding.passwordEt.text.toString().length == 8
+                    && binding.passwordEt.text.toString()
+                        .contains(Regex("(?=.*[a-z])(?=.*[0-9])(?=.*[A-Z])"))
+                ) {
+
+                    binding.getStarted.setBackgroundResource(R.drawable.sign_up_btn)
+                }
+
+
+            }
+
+        }
+
+
+
+
+
+        binding.passwordEt.addTextChangedListener(textWatcher)
+        binding.emailEt.addTextChangedListener(textWatcher)
+
+
+
+
+        return view
+
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment CreateAccount.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            CreateAccount().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
+
 }
