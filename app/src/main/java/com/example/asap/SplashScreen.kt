686dae7f259630499ed9bb3995d0bff93a935400
gideonjon <gideonjones63@gmail.com>
2024-10-25 11:53:12 +0100

    added-newly

--- app/src/main/java/com/example/asap/SplashScreen.kt
+++ app/src/main/java/com/example/asap/SplashScreen.kt
@@ -5,15 +5,27 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
+import com.example.asap.databinding.FragmentSplashScreenBinding
 
 
 class SplashScreen : Fragment() {
+    private var _binding : FragmentSplashScreenBinding? = null
+    private val binding get() = _binding!!
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_splash_screen, container, false)
+
+        _binding = FragmentSplashScreenBinding.inflate(inflater,container,false)
+        val view = binding.root
+
+
+
+
+        return view
+
     }
 
 
